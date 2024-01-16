044e2280a0c55ffa5bc9c7a4d8f04420c00d99f3
cr0c0 <hyde69ciel@gmail.com>
2024-01-16 21:24:17 +0900

    #240116

---
+++ src/main/java/me/croco/springbootdeveloper/TestService.java
@@ -0,0 +1,16 @@
+package me.croco.springbootdeveloper;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    @Autowired
+    MemberRepository memberRepository;  // 빈 주입
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();  // 멤버 목록 얻기
+    }
+}
