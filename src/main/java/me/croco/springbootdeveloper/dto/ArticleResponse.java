a6ce064ad34601f25e14eb3ff900670895abb002
cr0c0 <hyde69ciel@gmail.com>
2024-01-18 22:09:50 +0900

    #240118

---
+++ src/main/java/me/croco/springbootdeveloper/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package me.croco.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.croco.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleResponse {
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+
+    }
+}
